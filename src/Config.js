d13afd15dbc62f6d9ff9df8849cb7c1506ec83ea
emilsundberg-h <emil.a.sundberg@gmail.com>
2020-04-09 16:04:09 +0200

    repo

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    appId: '1279d453-6fbc-4701-a167-3d269be4a5a9',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'calendars.read'
+    ]
+  };
